d7dd12879cd6837e705535d7f5ea58cb286f6f8c
DylanJakeComilang <dylanjakecomilang@gmail.com>
2025-03-17 10:31:54 -0900

    Fixed Integrated States

--- src/main/java/frc/robot/commands/IntegratedStates/HighDealgifyCommand.java
+++ src/main/java/frc/robot/commands/IntegratedStates/HighDealgifyCommand.java
@@ -1,13 +1,14 @@
 package frc.robot.commands.IntegratedStates;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AlgaePivotStates.Descore;
 import frc.robot.commands.ElevatorStates.L4State;
 import frc.robot.commands.ElevatorStates.StorageState;
 import frc.robot.subsystems.AlgaePivot;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-public class HighDealgifyCommand extends ParallelCommandGroup {
+public class HighDealgifyCommand extends SequentialCommandGroup {
 
   public HighDealgifyCommand(ElevatorSubsystem elevatorSubsystem, AlgaePivot algaePivotSubsystem) {
     addCommands(
--- src/main/java/frc/robot/commands/IntegratedStates/LowDealgifyCommand.java
+++ src/main/java/frc/robot/commands/IntegratedStates/LowDealgifyCommand.java
@@ -1,13 +1,14 @@
 package frc.robot.commands.IntegratedStates;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AlgaePivotStates.Descore;
 import frc.robot.commands.ElevatorStates.L3State;
 import frc.robot.commands.ElevatorStates.StorageState;
 import frc.robot.subsystems.AlgaePivot;
 import frc.robot.subsystems.ElevatorSubsystem;
 
-public class LowDealgifyCommand extends ParallelCommandGroup {
+public class LowDealgifyCommand extends SequentialCommandGroup {
 
   public LowDealgifyCommand(ElevatorSubsystem elevatorSubsystem, AlgaePivot algaePivotSubsystem) {
   
