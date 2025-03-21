19ed3ad40275d91aac2a95ae9245e295b834f879
DylanJakeComilang <dylanjakecomilang@gmail.com>
2025-03-21 13:17:37 -1000

    Change tuck and lower high descore

--- src/main/java/frc/robot/commands/ElevatorStates/HighDescoreState.java
+++ src/main/java/frc/robot/commands/ElevatorStates/HighDescoreState.java
@@ -20,7 +20,7 @@ public class HighDescoreState extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    elevatorSub.setSetpoint(42);
+    elevatorSub.setSetpoint(35);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
